7d44a54c0b9ac2f36afd15ab79d92ebc8d4635b6
Can <kfjincan@gmail.com>
2015-01-22 21:05:52 -0500

    bnr cocoa chapter 15

---
+++ bnr_ch15ex/deprecated/RaiseMan/RaiseMan/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  RaiseMan
+//
+//  Created by Can on 1/13/15.
+//  Copyright (c) 2015 Can. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}

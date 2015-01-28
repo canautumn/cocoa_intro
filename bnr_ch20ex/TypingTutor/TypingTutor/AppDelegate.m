7e9f65beb12edb34c887f75a8fd92ebd2f8b5afc
Can <kfjincan@gmail.com>
2015-01-28 03:04:50 -0500

    BNR Cocoa Chapter 20

---
+++ bnr_ch20ex/TypingTutor/TypingTutor/AppDelegate.m
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.m
+//  TypingTutor
+//
+//  Created by Can on 1/26/15.
+//  Copyright (c) 2015 Can. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "BigLetterView.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+
+- (IBAction)boldClick:(id)sender {
+    [self.bigLetterView drawStringCenteredIn:self.bigLetterView.bounds];
+}
+
+@end
